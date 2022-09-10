1537a5b1cb87c06818e09e9d837b47505e44f1f6
mechztcc <dojathan@gmail.com>
2022-09-10 17:07:44 -0400

    feat: added templateOutlet to show dinamic content on modal

--- src/app/shared/components/modal/modal/modal.component.ts
+++ src/app/shared/components/modal/modal/modal.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { ModalConfig } from '../types/modal-config.interface';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
+  config: ModalConfig;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
