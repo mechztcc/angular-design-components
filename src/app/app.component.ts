2ce9d73b5da24b2f96a0b83eb6b3816a3ac15917
mechztcc <dojathan@gmail.com>
2022-09-10 16:33:56 -0400

    feat: added modal service on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {
+  ModalRef,
+  ModalService
+} from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('modal') modalTemplateRef: TemplateRef<any>;
+
   title = 'a11y-p2';
+  firstName = 'Alberto';
+  modalRef: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  show(): void {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User details',
+    });
+  }
 }
